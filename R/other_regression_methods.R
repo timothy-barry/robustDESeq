8e695cf7483287ee7eb02df5332947cab17d55b3
timothy-barry <tbarry2@andrew.cmu.edu>
2024-08-23 12:37:50 -0700

    add linear mode

--- R/other_regression_methods.R
+++ R/other_regression_methods.R
@@ -48,3 +48,26 @@ run_regress_out_covariates_test <- function(Y_list, x, Z, side = "two_tailed", h
                      rejected = result$rejected,
                      hyp_idx = seq_len(m)) |> dplyr::arrange(p_value)
 }
+
+
+#' Run linear model
+#'
+#' @inheritParams run_robust_nb_regression
+#'
+#' @return a data frame containing the p-values and rejections
+#' @export
+run_linear_model <- function(Y_list, x, Z, side = "two_tailed", alpha = 0.1) {
+  if (!(side %in% c("left", "right", "two_tailed"))) stop("`side` not recognized.")
+  p_values <- sapply(X = Y_list, FUN = function(y) {
+    # normalize y
+    log_y <- log(y + 1)
+    y_norm <- (log_y - mean(log_y))/sd(log_y)
+    fit <- stats::lm(y_norm ~ x + Z)
+    s <- summary(fit)
+    t <- coef(s)["x", "t value"]
+    deg_of_freedom <- fit$df.residual
+    compute_t_dist_p_value(t, deg_of_freedom, side)
+  })
+  rejected <- stats::p.adjust(p_values, method = "BH") < alpha
+  get_result_df(p_values, rejected)
+}
--- R/utilities.R
+++ R/utilities.R
@@ -5,6 +5,12 @@ compute_gaussian_p_value <- function(z, side) {
          two_tailed = 2 * pnorm(abs(z), lower.tail = FALSE))
 }
 
+compute_t_dist_p_value <- function(t, deg_of_freedom, side) {
+  switch(side,
+         left = pt(q = t, df = deg_of_freedom, lower.tail = TRUE),
+         right = pt(q = t, df = deg_of_freedom, lower.tail = FALSE),
+         two_tailed = 2 * pt(q = abs(t), df = deg_of_freedom, lower.tail = FALSE))
+}
 
 get_result_df <- function(p_values, rejected) {
   tibble::tibble(p_value = p_values,
@@ -12,12 +18,10 @@ get_result_df <- function(p_values, rejected) {
                  hyp_idx = seq_len(length(p_values))) |> dplyr::arrange(p_value)
 }
 
-
 get_side_code <- function(side) {
   if (!(side %in% c("left", "right", "two_tailed"))) stop("`side` not recognized.")
   switch(side, left = -1L, right = 1L, two_tailed = 0L)
 }
-
 
 get_result_metrics <- function(result, under_null) {
   n_true_discoveries <- sum(!under_null[result |> dplyr::filter(rejected) |> dplyr::pull(hyp_idx)])
