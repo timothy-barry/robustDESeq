7c7458c574d807f1750dceaacf5c19d1f39685bc
timothy-barry <tbarry2@andrew.cmu.edu>
2024-10-09 17:34:28 -0400

    update compute fdp funct

--- R/utilities.R
+++ R/utilities.R
@@ -12,24 +12,16 @@ compute_t_dist_p_value <- function(t, deg_of_freedom, side) {
          two_tailed = 2 * pt(q = abs(t), df = deg_of_freedom, lower.tail = FALSE))
 }
 
-get_result_df <- function(p_values, rejected, order_result_df) {
-  out <- tibble::tibble(p_value = p_values,
-                        rejected = rejected,
-                        hyp_idx = seq_len(length(p_values)))
-  if (order_result_df) out <- out |> dplyr::arrange(p_value)
-  return(out)
-}
-
 get_side_code <- function(side) {
   if (!(side %in% c("left", "right", "two_tailed"))) stop("`side` not recognized.")
   switch(side, left = -1L, right = 1L, two_tailed = 0L)
 }
 
 get_result_metrics <- function(result, under_null) {
-  n_true_discoveries <- sum(!under_null[result |> dplyr::filter(rejected) |> dplyr::pull(hyp_idx)])
-  discoveries <- result |> dplyr::filter(rejected) |> dplyr::pull(hyp_idx)
-  n_discoveries <- length(discoveries)
-  fdp <- if (n_discoveries >= 1L) mean(under_null[discoveries]) else 0
+  rejected <- result$rejected
+  n_true_discoveries <- sum(!under_null[rejected])
+  n_discoveries <- sum(rejected)
+  fdp <- if (n_discoveries >= 1L) mean(under_null[rejected]) else 0
   return(list(n_true_discoveries = n_true_discoveries, n_discoveries = n_discoveries, fdp = fdp))
 }
 
